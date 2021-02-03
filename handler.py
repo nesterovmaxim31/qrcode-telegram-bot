b002351fcf956bcf2017517d58a23bc8f5180af8
maxim pampam <maxgoonfuture@gmail.com>
2021-02-03 21:29:38 +0300

    make it better

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 from app import bot, db
 from aiogram.types import Message
 from config import admin_id
-from qrcode_lok import qrcode_loker, delete_file_photo_user
+# from qrcode_lok import qrcode_loker, delete_file_photo_user
 
 
 async def send_to_admin(db):
@@ -13,10 +13,10 @@ async def process_hi6_command(message: Message):
     await bot.send_message(chat_id=message.chat.id, text="Привет")
 
 
-@db.message_handler(content_types=['photo'])
-async def handle_docs_photo(message: Message):
-    await message.photo[0].download(f'test{message.from_user.id}.jpg')
-    await bot.send_message(message.from_user.id, text="Ожидайте результат...")
-    M = await qrcode_loker(message.from_user.id)
-    await bot.send_message(message.from_user.id, text=f"{M}")
-    await delete_file_photo_user(message.from_user.id)
+# @db.message_handler(content_types=['photo'])
+# async def handle_docs_photo(message: Message):
+#     await message.photo[0].download(f'test{message.from_user.id}.jpg')
+#     await bot.send_message(message.from_user.id, text="Ожидайте результат...")
+#     M = await qrcode_loker(message.from_user.id)
+#     await bot.send_message(message.from_user.id, text=f"{M}")
+#     await delete_file_photo_user(message.from_user.id)
