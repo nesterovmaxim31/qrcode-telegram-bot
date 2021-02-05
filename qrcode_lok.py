4cfe4c7de919bb8bf0fee4b1ac3b686b9e0787a9
maxim pampam <maxgoonfuture@gmail.com>
2021-02-05 18:40:19 +0300

    wregb

--- qrcode_lok.py
+++ qrcode_lok.py
@@ -1,21 +1,21 @@
-# import cv2
-# import os
-# from pyzbar import pyzbar
-#
-#
-# async def qrcode_loker(user_id):
-#     img = cv2.imread(f"test{user_id}.JPG")
-#     code = pyzbar.decode(img)
-#     D = 0
-#     for h in code:
-#         D = h.data.decode("utf-8")
-#     if not D:
-#         return "К сожалению мы не смогли расшифровать ваш qrcode.\nПопробуйте сделать повторное фото с" \
-#                " масимально точность"
-#     else:
-#         return D
-#
-#
-# async def delete_file_photo_user(user_id):
-#     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'test{user_id}.jpg')
-#     os.remove(path)
+import cv2
+import os
+from pyzbar import pyzbar
+
+
+async def qrcode_loker(user_id):
+    img = cv2.imread(f"test{user_id}.JPG")
+    code = pyzbar.decode(img)
+    D = 0
+    for h in code:
+        D = h.data.decode("utf-8")
+    if not D:
+        return "К сожалению мы не смогли расшифровать ваш qrcode.\nПопробуйте сделать повторное фото с" \
+               " масимально точность"
+    else:
+        return D
+
+
+async def delete_file_photo_user(user_id):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'test{user_id}.jpg')
+    os.remove(path)
